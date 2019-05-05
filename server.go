9f1626bb92851ce3d2841a7761b2d6fb6fdaa025
Matt Harrison <hi@matt-harrison.com>
2019-05-05 21:23:28 +0100

    port

--- server.go
+++ server.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	http.HandleFunc("/api/solve", solveHandle)
 
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
 func solveHandle(w http.ResponseWriter, r *http.Request) {
